f3585eb99b3aee5fd23668d7ba1b6d621d4b701e
dimas sudjito <dimassudjito@localhost.localdomain>
2021-04-15 22:59:37 -0600

    finish menu:

---
+++ menu/src/components/Categories.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="btn-container">
+        <div :key="category" v-for="category in categories">
+            <button
+            class="filter-btn"
+            @click="$emit('filter-items', category)"
+            >
+                {{category}}
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Categories",
+    props: {
+        categories: Array,
+    }
+}
+</script>
---
+++ menu/src/components/Menu.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="section-center">
+        <div :key="menuItem.id" v-for="menuItem in menuItems">
+            <article class="menu-item">
+                <img :src="menuItem.img" class="photo"/>
+                <div class="item-info">
+                    <header>
+                        <h4>{{menuItem.title}}</h4>
+                        <h4 class="price">${{menuItem.price}}</h4>
+                    </header>
+                        <p class="item-text">{{menuItem.desc}}</p>
+                </div>
+            </article>
+        </div> 
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        menuItems: Array,
+    },
+}
+</script>
