889e1bf2db3ce4a565e2df8529503de40455a034
dimas sudjito <dimassudjito@localhost.localdomain>
2021-04-30 12:06:02 -0600

    todo with vuex

--- todo/src/views/About.vue
+++ todo/src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
+  <div class="about pa-6">
     <h1>About vuetify todo</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda
+      deserunt voluptate quae, corporis nam tempore fugit blanditiis aspernatur
+      voluptatibus, praesentium tenetur facere odit dolor perferendis architecto
+      quaerat non corrupti hic?
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda
+      deserunt voluptate quae, corporis nam tempore fugit blanditiis aspernatur
+      voluptatibus, praesentium tenetur facere odit dolor perferendis architecto
+      quaerat non corrupti hic?
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda
+      deserunt voluptate quae, corporis nam tempore fugit blanditiis aspernatur
+      voluptatibus, praesentium tenetur facere odit dolor perferendis architecto
+      quaerat non corrupti hic?
+    </p>
   </div>
 </template>
