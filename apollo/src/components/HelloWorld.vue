96aad4e63f778320defadf9920b8f1ec8840efcd
Dimas Sudjito <almuhtad@ualberta.ca>
2021-06-09 20:18:16 -0600

    uh

---
+++ apollo/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <div v-for="country in countries" :key="country.name">
+      {{ country.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+
+export default {
+  name: 'HelloWorld',
+  apollo: {
+    countries: gql`
+      query {
+        countries {
+          name
+        }
+      }
+    `
+  }
+}
+</script>
